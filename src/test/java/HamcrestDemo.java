c64d98ca418ede85b09b8f85696487d1a38a6cf2
Mateusz Tadla <mtadla@dtiq.com>
2024-03-23 13:55:48 +0100

    init repo

---
+++ src/test/java/HamcrestDemo.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+
+public class HamcrestDemo {
+
+    @ValueSource(ints = {10, 11})
+    @ParameterizedTest()
+    public void checkNumber(int i){
+        assertThat(i, is(equalTo(10)));
+    }
+}
